4507b367ecf219bacf1c3f37e69ca1ea2e637c73
saifur550 <isaifur550@icloud.com>
2021-09-24 15:22:10 +0800

    update

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,16 +3,28 @@ import './Cart.css'
 
 const Cart = (props) => {
     const{cart} = props;
+
     let total = 0 ;
     for(const product of cart){
         total = total + product.price;
     }
+
+    const shipping = total>0 ? 15: 0;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total + shipping + tax;
+
+    // const total = cart.reduce(( pre, product)=> pre + product.price,0 )
+
     return (
-        <div>
+        <div className="main-cart">
             <h3>Order Summary</h3> 
               <h5>Item order : {props.cart.length}</h5> 
               <br/>
-            <p>Price : {total}</p>
+            <p>Price : {total.toFixed(2)}</p>
+            <p>shipping : {shipping}</p>
+            <p>Tax : {tax.toFixed(2)}</p>
+            <hr/>
+            <p>Grand Total  : {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
     // console.log(props);
     const {name, img, price ,stock ,seller} = props.product;
+    const icon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
             <div className="product-left">
@@ -14,7 +17,7 @@ const Product = (props) => {
             <p><small> By " {seller}</small></p>
             <h4>Price : {price}</h4>
             <p>only {stock} left in stock - order soon</p>
-            <button onClick={()=> props.handleAddToCart(props.product)} className="btn-regular">Add to Cart</button>
+            <button onClick={()=> props.handleAddToCart(props.product)} className="btn-regular"> {icon} Add to Cart</button>
             </div>
         </div>
     );
